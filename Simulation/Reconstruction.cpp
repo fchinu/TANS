ba3ea4b7c1f31f2a222bbde366fe1f0cf48bad39
Bizzzio <fachi2001@gmail.com>
2023-01-03 10:55:07 +0100

    Reconstruction using minimum DCA technique

--- Simulation/Reconstruction.cpp
+++ Simulation/Reconstruction.cpp
@@ -8,21 +8,34 @@ ClassImp(Reconstruction)
 Reconstruction::Reconstruction()
 {
     // Open the file containing the tree.
-    TFile* file = new TFile("Simulation/Tree.root","update");
-    TTreeReader myReader("fTreeRec", file);
+    TFile* file = TFile::Open("Simulation/Tree.root");
+    TTreeReader RecReader("fTreeRec", file);
    
     // The branch "RecHits detector 1" contains MaterialBudget::fPoint.
 
-    TTreeReaderValue<std::vector<MaterialBudget::fPoint>> intersect1(myReader, "RecHits_1");
-    TTreeReaderValue<std::vector<MaterialBudget::fPoint>> intersect2(myReader, "RecHits_2");
+    TTreeReaderValue<std::vector<MaterialBudget::fPoint>> intersect1(RecReader, "RecHits_1");
+    TTreeReaderValue<std::vector<MaterialBudget::fPoint>> intersect2(RecReader, "RecHits_2");
  
     // Loop over all entries of the TTree or TChain.
-    while (myReader.Next()) {
+    while (RecReader.Next()) {
        // Just access the data as if intersect1 and intersect2 were iterators (note the '*'
        // in front of them):
        fIntersections1.push_back(*intersect1);
        fIntersections2.push_back(*intersect2);
     }
+
+    TTreeReader GenReader("fTreeGen", file);
+   
+    TTreeReaderValue<std::vector<Event::fVertMult>> configs(GenReader, "Config");
+ 
+    // Loop over all entries of the TTree or TChain.
+    while (GenReader.Next()) {
+       // Just access the data as if intersect1 and intersect2 were iterators (note the '*'
+       // in front of them):
+       fConfigs.push_back(*configs);
+    }
+
+
 
     //cout << "Intersezioni1: " << fIntersections1.size() << endl;    
     //for (auto i: fIntersections1){
@@ -44,12 +57,17 @@ Reconstruction::Reconstruction()
     //cout << endl;
     //cout << "Fine stampa" << endl;
     
-
+    
     FindTracklets();
-    //cout << "Fine FindTracklets ended" << endl;
+    cout << "Fine FindTracklets ended" << endl;
     MinDca();
-    //cout << "VertexReco ended" << endl;
+    cout << "Fine MinDca" << endl;
+    FillHistoResiduals();
+    cout << "FillHIstoResiduals ended" << endl;
     file->Close();
+    TFile outfile("outfile.root","recreate");
+    fResiduals->Write();
+    outfile.Close();
 }
  
 void Reconstruction::FindTracklets()
@@ -57,18 +75,19 @@ void Reconstruction::FindTracklets()
     //cout << "Entering FindTracklets" << endl;
     double phimax = 0.010; // maximum angle difference for 2 intersections to be a tracklet
     std::vector<MaterialBudget::fPoint> tracklet;
-    for (auto x: fIntersections1)
-        for (auto i: fIntersections2)
-        {
-            for (auto y: x)
-                for (auto j: i)
-                    if(abs(y.phi-j.phi)<phimax)
-                    {
-                        tracklet.push_back(y);
-                        tracklet.push_back(j);
-                    }
+    for (unsigned i=0; i<fIntersections1.size(); ++i)
+    {
+        for (auto y: fIntersections1[i])
+            for (auto j: fIntersections2[i])
+                if(abs(y.phi-j.phi)<phimax)
+                {
+                    tracklet.push_back(y);
+                    tracklet.push_back(j);
+                }
+        if (tracklet.size()>0)
             fTracklets.push_back(tracklet);
-        }
+        tracklet.clear();
+    }
 }
  
 void Reconstruction::VertexReco()
@@ -109,38 +128,47 @@ void Reconstruction::VertexReco()
 void Reconstruction::MinDca()
 {
     TH1D* histo = new TH1D("vertex", "vertex", 50, -15,15);
-    vector<double> vertex;
-    for(auto& i: fTracklets){
-        for (unsigned j=0;j<i.size();j+=2)
-        {
+    vector<double> vertexTemp;
+    for(auto& i: fTracklets)
+    {
+        FillHistoMinDca(histo, i, vertexTemp);
+
+        int histomax(histo->GetMaximumBin()), count(0);
+        double xmin(histo->GetBinLowEdge(histomax)), xmax=xmin+histo->GetBinWidth(histomax), mean(0);
+        
+        for (auto& i : vertexTemp)
+            if (i<xmax && i>xmin)
+            {
+                mean+=i;
+                ++count;
+            }
+        fVertexesZ.push_back(mean/count);
+        cout<<"Vertex is:"<<fVertexesZ.back();
+        histo->Reset();
+        vertexTemp.clear();
+    }
+    delete histo;
+}
+
+void Reconstruction::FillHistoMinDca(TH1D* histo, vector<MaterialBudget::fPoint>& tracklets, vector<double>& vertextemp)
+{
+    for (unsigned j=0;j<tracklets.size();j+=2)
+    {
         double a,b,c; // parameters for line connecting two intersections of the same tracklet from detector 2 to detector 1
-        a = i[j+1].x - i[j].x;
-        b = i[j+1].y - i[j].y;
-        c = i[j+1].z - i[j].z;
+        a = tracklets[j+1].x - tracklets[j].x;
+        b = tracklets[j+1].y - tracklets[j].y;
+        c = tracklets[j+1].z - tracklets[j].z;
 
         double t;
-        t = - (a*i[j].x + b*i[j].y) / (a*a+b*b);    //t of closest approach
+        t = - (a*tracklets[j].x + b*tracklets[j].y) / (a*a+b*b);    //t of closest approach
 
-        vertex.push_back(i[j].z + c*t);
-        histo->Fill(vertex.back());
-        }
+        vertextemp.push_back(tracklets[j].z + c*t);
+        histo->Fill(vertextemp.back());
     }
-    TCanvas* c = new TCanvas("canvas","canvas",1000,1000);
-    histo->DrawClone("histo");
-    int histomax(histo->GetMaximumBin());
-    double xmin(histo->GetBinLowEdge(histomax)), xmax=xmin+histo->GetBinWidth(histomax);
-    double mean=0;
-    int count=0;
-    for (auto& i : vertex)
-    {
-        if (i<xmax && i>xmin)
-        {
-            mean+=i;
-            ++count;
-        }
-    }
-    mean/=count;
-    cout<<"Vertex is:"<<mean;
-    c->SaveAs("Canvas.png");
-    //delete histo;
+}
+
+void Reconstruction::FillHistoResiduals()
+{
+    for(unsigned i=0; i<fVertexesZ.size(); ++i)
+        fResiduals->Fill(fVertexesZ[i]-fConfigs[i][0].z);
 }
--- Simulation/Reconstruction.h
+++ Simulation/Reconstruction.h
@@ -12,6 +12,7 @@ class Reconstruction: public TObject
 {
 public:
     Reconstruction();
+    ~Reconstruction(){delete fResiduals;}
  
     void VertexReco();
     void FindTracklets();
@@ -20,8 +21,15 @@ public:
 private:
     std::vector<std::vector<MaterialBudget::fPoint>> fIntersections1;
     std::vector<std::vector<MaterialBudget::fPoint>> fIntersections2;
+    std::vector<std::vector<Event::fVertMult>> fConfigs;
     std::vector<std::vector<MaterialBudget::fPoint>> fTracklets;
     std::vector<std::vector<double>> fVertexes;
+    std::vector<double> fVertexesZ;
+    TH1D* fResiduals = new TH1D("Residuals", "Residuals", 500,-0.5,0.5);
+
+    void FillHistoMinDca(TH1D* histo, vector<MaterialBudget::fPoint>& tracklets, vector<double>& vertextemp);
+    void FillHistoResiduals();
+
  
     ClassDef(Reconstruction, 1)
 };
