a94e52e19de6ee63f0d3edc395130efc13caa24d
Bizzzio <fachi2001@gmail.com>
2023-01-08 19:11:19 +0100

    Added seed choice

--- Simulation/Simulation.cpp
+++ Simulation/Simulation.cpp
@@ -2,16 +2,6 @@
 
 void Simulation(TString ConfigFileName = "Config_Run.yaml")
 {
+    gRandom->SetSeed(42);
     Run a(ConfigFileName);
-    //TFile hfile("Tree.root","recreate");
-    //TTree tree("tree","tree");
-    //std::vector<double> numbers = {1.,2.,3.};
-    //std::vector<double>* ptr=&numbers;
-    //tree.Branch("numbers",&ptr);
-    //tree.Fill();
-    //std::vector<double> numbers2 = {10.,20.,30.};
-    //ptr=&numbers2;
-    //tree.Fill();
-    //hfile.Write();
-    //hfile.Close();
 }
