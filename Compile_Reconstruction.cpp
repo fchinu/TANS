7c73165e810042c73f58b27816b941afd25faa08
Bizzzio <fachi2001@gmail.com>
2023-01-16 19:28:01 +0100

    Updated

--- Compile_Reconstruction.cpp
+++ Compile_Reconstruction.cpp
@@ -4,20 +4,20 @@
 
 void Compile_Reconstruction(TString myopt="run")
 {
-    //gSystem->AddIncludePath((string("-I ")+gSystem->GetWorkingDirectory()+"/Simulation").c_str());
-    //gSystem->AddIncludePath((string("-I ")+gSystem->GetWorkingDirectory()+"/Reconstruction").c_str());
-    //gSystem->AddIncludePath((string("-I ")+gSystem->GetWorkingDirectory()+"/Build").c_str());
+    gSystem->AddIncludePath((string("-I ")+gSystem->GetWorkingDirectory()+"/Simulation").c_str());
+    gSystem->AddIncludePath((string("-I ")+gSystem->GetWorkingDirectory()+"/Reconstruction").c_str());
+    gSystem->AddIncludePath((string("-I ")+gSystem->GetWorkingDirectory()+"/Build").c_str());
     TString opt;
     if (myopt.Contains("force"))
-        opt="kfg";          //k mantiene il .so, f forza la compilazione (come mettere il ++), g serve se vogliamo usare un debugger
+        opt="kfg-";          //k mantiene il .so, f forza la compilazione (come mettere il ++), g serve se vogliamo usare un debugger
     else
-        opt="kg";
+        opt="kg-";
     if (myopt.Contains("clean"))
         gSystem->Exec("./clean.sh");
-    gSystem->CompileMacro("Simulation/MaterialBudget.cpp",opt.Data());
-    gSystem->CompileMacro("Simulation/Event.cpp",opt.Data());
-    gSystem->CompileMacro("Reconstruction/Reconstruction.cpp",opt.Data());
-    gSystem->CompileMacro("Reconstruction/RunAnalysis.cpp",opt.Data());
-    if (myopt.Contains("run"))
-        gROOT->ProcessLine("RunAnalysis()");
+    gSystem->CompileMacro("Simulation/MaterialBudget.cpp",opt.Data(),"","Build");
+    gSystem->CompileMacro("Simulation/Event.cpp",opt.Data(),"","Build");
+    gSystem->CompileMacro("Reconstruction/Reconstruction.cpp",opt.Data(),"","Build");
+    gSystem->CompileMacro("Reconstruction/RunAnalysis.cpp",opt.Data(),"","Build");
+
+    gROOT->ProcessLine("RunAnalysis()");
 }
